63bedf5d84bbbb2387dfcf7426ad8b5dfb5ca071
Emilio Javier Mitre <ejmitre97@gmail.com>
2024-12-04 01:32:22 -0300

    AFP 4  App 4.4

--- AFP_4_TDII_2024/AFP_4_Grupo_8_2024_App_4.4/Core/Src/main.c
+++ AFP_4_TDII_2024/AFP_4_Grupo_8_2024_App_4.4/Core/Src/main.c
@@ -13,6 +13,9 @@
   * in the root directory of this software component.
   * If no LICENSE file comes with this software, it is provided AS-IS.
   *
+  * Descripcion App4.2: Desarrolle una aplicacion que utilice el pulsador onboard de la placa de desarrollo para alternar entre dos secuencias diferentes.
+  * La aplicacion inicia con la secuencia de la App1.1 y, cuando se presione el pulsador, dicha secuencia debe invertirse y continuar, de manera que, cada
+  * vez que se presione el pulsador la secuencia actual se invierta. La aplicacion debe ser de caracter general.
   ***************************************************************************************************************************************************************
   */
 /* USER CODE END Header */
@@ -27,6 +30,12 @@
 /* USER CODE END Includes */
 
 /* Private typedef ---------------------------------------------------------------------------------------------------------------------------------------------*/
+typedef enum{				//Se definen dos constantes enum para las secuencias y se utiliza typedef para asignarle el alias secuencia
+	secuencia_1=1,
+	secuencia_2=2,
+	secuencia_3=3,
+	secuencia_4=4
+} secuencia;
 /* USER CODE BEGIN PTD */
 
 /* USER CODE END PTD */
@@ -43,19 +52,19 @@
 
 /* Private variables ------------------------------------------------------------------------------------------------------------------------------------------*/
 /* USER CODE BEGIN PV */
-uint16_t LED[N]={LD1_Pin, LD2_Pin, LD3_Pin};										//vector de leds
-delay_t delay1, delay2, delay3, delay4, delay5;
-uint16_t delay[5]={100, 150, 300, 600};												//vector para distintos tiempos de espera funcion delay no bloqueante
-uint8_t estado_boton=0;
-uint8_t led_actual=0;
-uint8_t secuencia=0;																//Numero de secuencia
-bool_t 	Flag_Sec4=0;
+uint16_t 	LED[N]={LD1_Pin, LD2_Pin, LD3_Pin};										//vector de leds
+secuencia 	estado_secuencia=secuencia_1;  											//el estado inicial sera la secuencia normal
+bool_t	 	estado_boton=0;
+delay_t delay1, delay2, delay3, delay4;
 /* USER CODE END PV */
 /* Private function prototypes ---------------------------------------------------------------------------------------------------------------------------------*/
+/* USER CODE BEGIN PFP */
 void SystemClock_Config(void);
 void MX_GPIO_Init(void);
-/* USER CODE BEGIN PFP */
-
+void secuencia1(void);
+void secuencia2(void);
+void secuencia3(void);
+void secuencia4(void);
 /* USER CODE END PFP */
 
 /* Private user code -------------------------------------------------------------------------------------------------------------------------------------------*/
@@ -70,11 +79,10 @@ bool_t readButton = true;
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-  delayInit(&delay1, delay[0]);														//200ms
-  delayInit(&delay2, delay[1]);														//150ms
-  delayInit(&delay3, delay[2]);														//300ms
-  delayInit(&delay4, delay[3]);														//600ms
-  delayInit(&delay5, delay[1]);														//150ms
+	delayInit(&delay1, 100);
+	delayInit(&delay2, 250);
+	delayInit(&delay3, 500);
+	delayInit(&delay4, 1000);													//600ms
   /* USER CODE END 1 */
 
   /* MCU Configuration-------------------------------------------------------------------------------------------------------------------------------------------*/
@@ -110,70 +118,64 @@ int main(void)
 	  readButton = readButton_GPIO(); 												// Lee estado actual del boton y lo guarda
 	  debounceFSM_update(readButton);
 	  if(readKey()){
-		 secuencia=(secuencia+1)%4;													// Cambia entre las 4 secuencias
+		 estado_secuencia=(estado_secuencia+1)%4;									// Cambia entre las 4 secuencias
 		 writeLedOff_GPIO(LED[0] | LED[1] | LED[2]);								// Se apagan todos los leds antes de comenzar una nueva secuencia
-		 led_actual=0;
-		 Flag_Sec4=0;
 	  }
-	  switch (secuencia)
-	  {
-	  	  case 0:																	//Secuencia 1 Parpadean en secuencia cada 150ms (verde, azul, rojo)
 
-	          if (delayRead(&delay2)){
-	        	  toggleLed_GPIO(LED[led_actual]);
-	          	  led_actual=(led_actual+1)%N;
-	          }
-	  	  	  break;
+	  /* Selecciona cual secuencia se ejecutara*/
+	  switch (estado_secuencia){
 
-	        case 1:																	//Secuencia 2 Parpadean los tres en simultaneo cada 300ms
-	  	  		if (delayRead(&delay3))
-	  	  		{
-	  	  			 toggleLed_GPIO(LED[0] | LED[1] | LED[2]);
-	  	  		}
-	  	  		break;
-
-	        case 2:																	// Secuencia 3 Parpadean Led Verde 100ms, Led Azul 300ms y Led Rojo 600ms
-	  	  		if (delayRead(&delay1))
-	  	  		{
-	  	  			toggleLed_GPIO(LED[0]);
-	  	  		}
-	  	  		if (delayRead(&delay3)){
-	  	  			toggleLed_GPIO(LED[1]);
-	  	  		}
-
-	  	  		if (delayRead(&delay4)){
-	  	  			toggleLed_GPIO(LED[2]);
-	  	  		}
-
-	  	  		break;
-
-	        case 3:																	// Secuencia 4 Parpadea Verde y Rojo en simultaneo alternado con Azul cada 150ms
-
-	        	if (delayRead(&delay2) && Flag_Sec4==0)								// En la primera vez que se ingresa a la secuencia se apaga el led2 y se enciende 1 y 3
-	  	  		{
-	  	  			writeLedOff_GPIO(LED[1]);
-	  	  			writeLedOn_GPIO(LED[0] | LED[2]);
-	  	  			Flag_Sec4=1;
-	  	  		}
-
-	  	  		if (delayRead(&delay5))
-	  	  		{
-	  	  			  toggleLed_GPIO(LED[0]	| LED[2]);
-	  	  			  toggleLed_GPIO(LED[1]);
-	  	  		}
-
-	  	  		break;
-	  	}
+	  	  case 1:
+	  		  	  	  secuencia1();
+	  		  	  	  break;
+	  	  case 2:
+	  		  		  secuencia2();
+	  		  		  break;
+	  	  case 3:
+	  		  		  secuencia3();
+	  		  		  break;
+	  	  case 4:
+	  		  		  secuencia4();
+	  		  		  break;
+	  }
   }
 /* USER CODE END WHILE */
 
   /* USER CODE BEGIN 3 */
 
   /* USER CODE END 3 */
-
 }
 /* USER CODE BEGIN 4 */
+// Funcion para hacer parpadear simultaneamente todos los leds cada 100ms
+void secuencia1(void){
+	if(delayRead(&delay1)){
+		toggleLed_GPIO( LED[0] | LED[1]| LED[2]);
+	}
+}
 
+// Funcion para hacer parpadear simultaneamente todos los leds cada 250ms
+void secuencia2(void){
+
+	if(delayRead(&delay2)){
+		toggleLed_GPIO( LED[0] | LED[1]| LED[2]);
+	}
+}
+
+// Funcion para hacer parpadear simultaneamente todos los leds cada 500ms
+void secuencia3(){
+
+	if(delayRead(&delay3)){
+		toggleLed_GPIO( LED[0] | LED[1]| LED[2]);
+	}
+}
+
+// Funcion para hacer parpadear simultaneamente todos los leds cada 1000ms
+void secuencia4(){
+
+	if(delayRead(&delay4)){
+		toggleLed_GPIO( LED[0] | LED[1]| LED[2]);
+	}
+}
 /* USER CODE END 4 */
 
 /**
